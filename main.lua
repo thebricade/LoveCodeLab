b54d4ad07be46b562c3fc031e12ca71d658823e2
Brianna <bs3864@nyu.edu>
2019-11-06 22:58:15 -0500

    Flashlight
    created a simple flashlight that will run out of battery & a function to get more battery

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 local playerSprite
+local flashlightSprite
 
 --there's a lot about event driven programming to add things to a event queue before doing them
 
@@ -17,6 +18,12 @@ function love.load()
 	playerX = 100
 	playerY = 500 
 
+	-- Flashlight ---
+	flashlightSprite = love.graphics.newImage("lover1.PNG") --just use same sprite for now
+	flashlightBattery = 100;
+	flashLightX = playerX; 
+	flashLightY = playerY; 
+
 	--made faux debug text-- 
 	text = "Nothing here"
 	counter = 0
@@ -30,20 +37,28 @@ end
 function love.draw()
 	PlayerConstructor()  -- is this only making one? or many?
 	--wait can I make a player and constructor that has this?
-	love.graphics.print(text, 300,300)
+	love.graphics.print(flashlightBattery, 300,300)
 end
 
 function PlayerConstructor()
 	love.graphics.draw(playerSprite,playerX,playerY) 
-	-- love.graphics.drawq which adds a canvis, quad and x, y)
+	-- love.graphics.drawq which adds a canvas, quad and x, y)
 		
+end
+
+function FlashlightConstructor()
+	love.graphics.draw(flashlightSprite,0,0)
 end
 
 function love.keypressed(key, isrepeat)
    
 --Player Controller-- 
    if key == "w" then  --did you know Keys are case sensitive and can't be uppercase D:< 
-      text = "W mouse button has been pressed"
+      	--check if we still have battery in the flashlight 
+      	if flashlightBattery > 0 then
+      		FlashlightConstructor()
+      		flashlightBattery = flashlightBattery - 1
+      	end
    elseif key == "a" then
    		playerX = playerX - 2 
    		--do something
