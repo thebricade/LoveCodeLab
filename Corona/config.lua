d84408ef8f05ed52ef60fc52510477ffc6197b2c
Brianna <bs3864@nyu.edu>
2019-11-11 11:51:16 -0500

    Adding Corona & Simple Run Test
    Tested to make sure I could run on Corona and got the engine all setup

---
+++ Corona/config.lua
@@ -0,0 +1,15 @@
+application = 
+{
+	content = 
+	{
+		fps = 60, 
+		width = 320, 
+		height = 480,
+		scale = "zoomStretch", -- if you don't want graphics stretching you can change to "letterbox"
+
+		imageSuffix = 
+		{
+			["@2x"] = 2; -- displays the resolutions @2x retina graphics where applicable
+		}
+	}
+}
---
+++ CoronaSimulator/config.lua
@@ -0,0 +1,21 @@
+--
+-- For more information on config.lua see the Project Configuration Guide at:
+-- https://docs.coronalabs.com/guide/basics/configSettings
+--
+
+application =
+{
+	content =
+	{
+		fps = 60,
+		width = 320,
+		height = 480, 
+		scale = "letterbox",
+			
+		imageSuffix =
+		{
+			    ["@2x"] = 2,
+			    ["@4x"] = 4,
+		}
+	}
+}
