528802006585b954b6cb70b740ac9be87e305720
Brianna <bs3864@nyu.edu>
2019-11-13 10:58:45 -0500

    Trying to get  image sprites to load into the game

--- CoronaSimulator/view.lua
+++ CoronaSimulator/view.lua
@@ -3,14 +3,37 @@
 ]]
 
 local view = { }
-
+_H = display.contentHeight; 
+_W = display.contentWidth; 
 --
 --		Initialise the model.
 --
 function view.initialise()
-																	-- initialise the score (part 4, exercise)
+	local screenText = display.newText( "You have" ,_W/2,_H/8, native.systemFontBold )	
+
+	local plotAnimation = 
+	{
+		name = "plot", 
+		frame = {1}
+		time = 0 
+		loopCount = 0 --if you were to have animation 
+	}
+
+	local circ = display.newCircle( 0, 0, 64 )
+	local plotSprite = display.newSprite( "plot.png", plotAnimation )
+	view.plots = {} -- array to store plant plots 
+	
+	for i=1,10 do
+		--view.plots[i] = 1
+		--view.view.plots[i] = graphics.newImageSheet( "plot.png", 100,100 )
+		--view.plots{i} = display.newImage("plot.png", 100,100)
+		print(i)
+	end
+
 
 end
+
+
 
 
 return view -- need to return
