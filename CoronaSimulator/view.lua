4718b6aa51325a72cc243b7461a631839038b49d
Brianna <bs3864@nyu.edu>
2019-11-13 15:02:31 -0500

    Grid is working
    it's spacing a bit strange but it is working to create a grid

--- CoronaSimulator/view.lua
+++ CoronaSimulator/view.lua
@@ -29,17 +29,29 @@ function view.initialze()
 	view.plots = {} -- array to store plant plots 
 
 	local GRID_SPACE = 60
+	local GRID_WIDTH = 5
+	local GRID_HEIGHT = 5
 	local currentX = 0 
 	
-	for i=1,10 do
+	for i=1,GRID_WIDTH do
+
+		for j=1,GRID_HEIGHT do
+			view.plots[i] = display.newImage("plot.png",30,30) -- for some reason this is no longer resizing the sprites
+			view.plots[i].x = _W/4 *i
+			view.plots[i].y = _H/3.5 *j
+			print(i)
+		end
+
+		--[[
 		view.plots[i] = display.newImageRect("plot.png",50,50) -- makes an image at 0,0
+		-- this is a for loop inside of a for loop 
 		view.plots[i].x = _W/4 + (i-1) * GRID_SPACE
 		view.plots[i].y = _H/3.5 
 
 		--view.plots[i] = 1
 		--view.view.plots[i] = graphics.newImageSheet( "plot.png", 100,100 )
 		--view.plots{i} = display.newImage("plot.png", 100,100)
-		print(i)
+		print(i)--]]
 	end
 
 
